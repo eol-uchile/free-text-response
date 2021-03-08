eec58e583bde6aa5098230e4c74a1e59fb9f7702
matsalinas <matsalinas@uchile.cl>
2021-03-08 20:10:42 +0000

    Update tests Koa

--- .github/test.sh
+++ .github/test.sh
@@ -2,14 +2,14 @@
 
 pip install -e /openedx/requirements/free-text-response
 
-cd /openedx/requirements/free-text-response/freetextresponse
+cd /openedx/requirements/free-text-response
 cp /openedx/edx-platform/setup.cfg .
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/free-text-response/freetextresponse
+cd /openedx/requirements/free-text-response
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest freetextresponse/tests.py
 
 rm -rf test_root
