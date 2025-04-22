2f4ec92defc6fdb6bdf5d701d04e71b2c1a45621
fsologureng <fsologur@uchile.cl>
2025-04-22 13:38:58 -0400

    refactor(test): simplifies `test.sh` script

--- .github/test.sh
+++ .github/test.sh
@@ -6,19 +6,10 @@ pip install --src /openedx/venv/src -e /openedx/requirements/app
 pip install pytest-cov genbadge[coverage]
 
 cd /openedx/requirements/app
-cp /openedx/edx-platform/setup.cfg .
-
-sed -i '/--json-report/c addopts = --nomigrations --reuse-db --durations=20 --json-report --json-report-omit keywords streams collectors log traceback tests --json-report-file=none --cov=freetextresponse/ --cov-report term-missing --cov-report xml:reports/coverage/coverage.xml --cov-fail-under 70' setup.cfg
 
 mkdir test_root
-cd test_root/
-ln -s /openedx/staticfiles .
-
-cd /openedx/requirements/app
+ln -s /openedx/staticfiles ./test_root/
 
 DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest freetextresponse/tests.py
 
-rm -rf test_root
-
-echo "[run]\nomit = freetextresponse/migrations/*" > .coverage
 genbadge coverage
