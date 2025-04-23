fb956d3131e0fe94a0d3cc24627930374ec45cdc
Lozamded <dhiguerafernandez@gmail.com>
2025-04-23 10:46:13 -0400

    FIX: Cache navigation fix

--- freetextresponse/freetextresponse.py
+++ freetextresponse/freetextresponse.py
@@ -314,7 +314,8 @@ class FreeTextResponse(
                 'word_count_message': self._get_word_count_message(),
                 'display_other_responses': display_other_responses,
                 'other_responses': self.get_other_answers(),
-                'image_path' : self.runtime.local_resource_url(self, "public/images/")
+                'image_path' : self.runtime.local_resource_url(self, "public/images/"),
+                'last_submission_time': self.last_submission_time.isoformat() if self.last_submission_time else '',
             }
         )
         template = self.loader.render_django_template(
